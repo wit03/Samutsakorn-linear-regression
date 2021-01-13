da75110a7a05947324b761eef39be5fe47601257
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2021-01-13 23:04:36 +0700

    :sparkles: - add predict function

--- regression.R
+++ regression.R
@@ -5,5 +5,11 @@ infect_relation = data.frame(
 
 infect_relation.model = lm(infected ~ date, data=infect_relation)
 summary(infect_relation.model)
-plot(infected~date, infect_relation)
+plot(infected~date, infect_relation, main="Samutsakorn Prediction Graph from 31 dec 2020 to 13 Jan 2021 ")
 abline(infect_relation.model)
+
+predictDate <- function(d) {
+ result <- predict.lm(infect_relation.model, data.frame(date = d))  
+ return(result)
+}
+predictDate(14)
